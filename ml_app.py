1e8841093716ef32d720a36f8ce3e55f65751a6e
pragyanaischool <75975970+pragyanaischool@users.noreply.github.com>
2024-04-16 22:08:21 +0530

    Update ml_app.py

--- ml_app.py
+++ ml_app.py
@@ -48,5 +48,5 @@ Lug_boot = st.number_input('lug_boot:', min_value=1, max_value=3, value=1)
 Safety = st.radio('safety:', ('med', 'high', 'low'))
 
 if st.button('Submit_Car_Infos'):
-    sale_price = predict(Buying, Maint, Doors, Persons, Lug_boot, Safety)
+    cal_eval = predict(Buying, Maint, Doors, Persons, Lug_boot, Safety)
     st.success(f'The Evaluation of Car : {car_eval[0]}')
